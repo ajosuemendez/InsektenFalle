281080473dfbb46cfc29cd1c65322b6c359a17c4
Alejandro Mendez <76664851+ajosuemendez@users.noreply.github.com>
2021-02-07 19:50:42 +0100

    Create Wind_Direction.h

---
+++ Wind_Direction.h
@@ -0,0 +1,16 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include <project.h>
+
+int Get_Wind_Direction();
+
+/* [] END OF FILE */
